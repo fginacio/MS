93b2f85e7d05456a2741f89102416fa2aba81e83
fginacio <124682770+fginacio@users.noreply.github.com>
2023-02-07 10:43:41 -0300

    Update TSSv2Collect1_2.ps1

--- TSSv2Collect1_2.ps1
+++ TSSv2Collect1_2.ps1
@@ -15,7 +15,6 @@ Function Invoke-TSSv2Collector{
 
 #$TSSv2 = C:\dell\TSSv2\TSSv2.ps1 -sdp
 $dell="c:\Dell\"
-#$removetss="C:\Dell\TSSv2"
 Clear-Host
 Write-Host "Downloading TSSv2..."
 mkdir c:\Dell -ErrorAction Ignore
