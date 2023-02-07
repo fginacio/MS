cf5ada0c7b1ef75c8a245fc65662a1dc2329a90f
fginacio <124682770+fginacio@users.noreply.github.com>
2023-02-07 19:59:15 -0300

    Update TSSv2Collect_DEV.ps1

--- TSSv2Collect_DEV.ps1
+++ TSSv2Collect_DEV.ps1
@@ -9,15 +9,17 @@ Function EndScript{
 Function Invoke-TSSv2Collect{
 #>
 
-#$TSSv2 = C:\dell\TSSv2\TSSv2.ps1 -sdp
 $dell="c:\Dell\"
 Clear-Host
 Write-Host "Downloading TSSv2..."
-Remove-Item "C:\Dell\SDP_*" -recurse -force -ErrorAction Ignore
-Remove-Item "C:\Dell\TSSv2Collect*.log" -recurse -force -ErrorAction Ignore
-mkdir c:\Dell -ErrorAction Ignore
-wget http://aka.ms/getTss -OutFile c:\Dell\TSSv2.zip
-Expand-Archive -Path c:\Dell\TSSv2.zip -DestinationPath c:\Dell\TSSv2\ -ErrorAction Ignore
+#Deleting old log collections and transcript logs
+    Remove-Item "C:\Dell\SDP_*" -recurse -force -ErrorAction Ignore
+    Remove-Item "C:\Dell\TSSv2Collect*.log" -recurse -force -ErrorAction Ignore
+#Creating c:\Dell folder and downloading TSSv2
+    mkdir c:\Dell -ErrorAction Ignore
+    wget http://aka.ms/getTss -OutFile c:\Dell\TSSv2.zip
+#Unpacking TSSv2 at C:\Dell
+    Expand-Archive -Path c:\Dell\TSSv2.zip -DestinationPath c:\Dell\TSSv2\ -ErrorAction Ignore
 Clear-Host
 $Ver="DEV2"
 
@@ -89,7 +91,7 @@ default {
 Stop-Transcript
 DisplayMenu
 $logfolder=(gci -Path c:\dell\SDP_* | ? { $_.PSIsContainer } | sort CreationTime).name
-Write-Host "Logs available at c:\Dell\SDP_$logfolder"
+Write-Host "Logs available at c:\Dell\$logfolder"
 #Removing extracted collector and zip file
     Remove-Item "C:\Dell\Tssv2" -recurse -force -ErrorAction Ignore
     Remove-Item "C:\Dell\TSSv2.zip" -recurse -force -ErrorAction Ignore
