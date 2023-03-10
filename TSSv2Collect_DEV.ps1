a593f3114c8273f989498c01f7567f2014736597
fginacio <124682770+fginacio@users.noreply.github.com>
2023-03-10 14:28:05 -0600

    Choose file name

--- TSSv2Collect_DEV.ps1
+++ TSSv2Collect_DEV.ps1
@@ -3,41 +3,19 @@
        TSSv2Collect.ps1
     .EXAMPLES
        Invoke-TSSv2Collect
+#>
 Function EndScript{ 
     break
 }
-Function Invoke-TSSv2Collect{
-#>
+#Function Invoke-TSSv2Collect{
 
-$dell="c:\Dell\"
-Clear-Host
-Write-Host "Downloading TSSv2..."
-#Deleting old log collections and transcript logs
-    Remove-Item "C:\Dell\SDP_*" -recurse -force -ErrorAction Ignore
-    Remove-Item "C:\Dell\TSSv2Collect*.log" -recurse -force -ErrorAction Ignore
-    Remove-Item "C:\Dell\Tssv2" -recurse -force -ErrorAction Ignore
-    Remove-Item "C:\Dell\TSSv2.zip" -recurse -force -ErrorAction Ignore
-#Creating c:\Dell folder and downloading TSSv2
-    mkdir c:\Dell -ErrorAction Ignore
-    wget http://aka.ms/getTss -OutFile c:\Dell\TSSv2.zip
-#Unpacking TSSv2 at C:\Dell
-    Expand-Archive -Path c:\Dell\TSSv2.zip -DestinationPath c:\Dell\TSSv2\ -ErrorAction Ignore
-Clear-Host
-$Ver="DEV2"
-
-#IE Fix
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
-
-#Set Execution Policy
-Set-ExecutionPolicy Unrestricted
-   
     function DisplayMenu {
     $DateTime=Get-Date -Format yyyyMMdd_HHmmss
     Start-Transcript -NoClobber -Path "C:\Dell\TSSv2Collect_$DateTime.log"
     Clear-Host
     Write-Host @"
     +===============================================+
-    |  TSSv2 - Log Collection  v$Ver Offline Mode   |     
+    |  TSSv2 - Log Collection  v$Ver                 |     
     |                                               |
     |           By: Fabiano Inacio                  | 
     +===============================================+
@@ -54,17 +32,17 @@ Switch ($MENU)
     {
         1 {
     #OPTION1 - Default Collection
-    C:\dell\TSSv2\TSSv2.ps1 -sdp Setup -LogFolderPath $dell -AcceptEula
-    #Write-Host "Logs available at C:\Dell\SDP_Setup\"
+    $path = Read-Host "Choose the logfile name or SupportCase Number"
+    invoke-expression -command "C:\dell\TSSv2\TSSv2.ps1 -sdp Mini -LogFolderPath $dell -AcceptEula -noZip"
+    cd $tss
+    Compress-Archive -path $dell$path -DestinationPath $dell
     $Shell = New-Object -ComObject "WScript.Shell"
     $Button = $Shell.Popup("Logs available at C:\Dell\SDP_Setup\", 0, "Collection Successfull", 0)
-    #Start-Sleep -Seconds 2
     DisplayMenu
   }
         2 {
     #OPTION2 - Cluster Collection
-    C:\dell\TSSv2\TSSv2.ps1 -sdp Cluster -LogFolderPath $dell -AcceptEula
-    #Write-Host "Logs available at c:\Dell\SDP_*"
+    invoke-expression -command "C:\dell\TSSv2\TSSv2.ps1 -sdp Cluster -LogFolderPath $dell -AcceptEula"
     $Shell = New-Object -ComObject "WScript.Shell"
     $Button = $Shell.Popup("Logs available at C:\Dell\SDP_Cluster\", 0, "Collection Successfull", 0)
     Start-Sleep -Seconds 2
@@ -72,8 +50,7 @@ Switch ($MENU)
   }
         3 {
     #OPTION3 - HyperV Collection
-    C:\dell\TSSv2\TSSv2.ps1 -sdp HyperV -LogFolderPath $dell -AcceptEula
-    #Write-Host "Logs available at c:\Dell\SDP_*"
+    invoke-expression -command "C:\dell\TSSv2\TSSv2.ps1 -sdp HyperV -LogFolderPath $dell -AcceptEula"
     $Shell = New-Object -ComObject "WScript.Shell"
     $Button = $Shell.Popup("Logs available at C:\Dell\SDP_HyperV\", 0, "Collection Successfull", 0)
     Start-Sleep -Seconds 2
@@ -82,8 +59,8 @@ Switch ($MENU)
         Q {
     #OPTIONQ - EXIT
     Write-Host "Bye"
-    Remove-Item "C:\Dell\Tssv2" -recurse
-    Break
+    Stop-Transcript
+    EndScript
     }
 default {
     #DEFAULT OPTION
@@ -92,12 +69,38 @@ default {
     DisplayMenu
         }
                     }
-}
-Stop-Transcript
+    }
+#}
+
+$dell="c:\Dell\"
+$TSS="C:\dell\TSSv2\"
+Clear-Host
+Write-Host "Downloading TSSv2..."
+#Deleting old log collections and transcript logs
+    Remove-Item "C:\Dell\SDP_*" -recurse -force -ErrorAction Ignore
+    Remove-Item "C:\Dell\TSSv2Collect*.log" -recurse -force -ErrorAction Ignore
+    Remove-Item "C:\Dell\Tssv2" -recurse -force -ErrorAction Ignore
+    Remove-Item "C:\Dell\TSSv2.zip" -recurse -force -ErrorAction Ignore
+#Creating c:\Dell folder and downloading TSSv2
+    mkdir c:\Dell -ErrorAction Ignore
+    wget http://aka.ms/getTss -OutFile c:\Dell\TSSv2.zip
+#Unpacking TSSv2 at C:\Dell
+    Expand-Archive -Path c:\Dell\TSSv2.zip -DestinationPath c:\Dell\TSSv2\ -ErrorAction Ignore
+Clear-Host
+$Ver="1.1"
+
+#IE Fix
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
+
+#Set Execution Policy
+Set-ExecutionPolicy Unrestricted
+
 DisplayMenu
+
+#Invoke-TSSv2Collect
+
 $logfolder=(gci -Path c:\dell\SDP_* | ? { $_.PSIsContainer } | sort CreationTime).name
 Write-Host "Logs available at c:\Dell\$logfolder"
 #Removing extracted collector and zip file
-    #Remove-Item "C:\Dell\Tssv2" -recurse -force -ErrorAction Ignore
-    #Remove-Item "C:\Dell\TSSv2.zip" -recurse -force -ErrorAction Ignore
+    Remove-Item "C:\Dell\Tssv2" -recurse -force -ErrorAction Ignore
 #}
