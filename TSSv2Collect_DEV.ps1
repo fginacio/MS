90c5ac6482ed7acb0e48f8f9364bed2df858a04a
fginacio <124682770+fginacio@users.noreply.github.com>
2023-03-14 17:01:19 -0500

    V 1.2

--- TSSv2Collect_DEV.ps1
+++ TSSv2Collect_DEV.ps1
@@ -89,7 +89,7 @@ Switch ($MENU)
         {
             $CaseNumber = ‘Mini Collection’
         }
-    invoke-expression -command "C:\dell\TSSv2\TSSv2.ps1 -Mini HyperV -LogFolderPath $dell -AcceptEula -noZip"
+    invoke-expression -command "C:\dell\TSSv2\TSSv2.ps1 -sdp Mini -LogFolderPath $dell -AcceptEula -noZip"
     cd $tss
     Compress-Archive -path "C:\Dell\SDP_Mini\" -DestinationPath $dell\$CaseNumber
     Remove-Item "C:\Dell\SDP_*" -recurse -force -ErrorAction Ignore
