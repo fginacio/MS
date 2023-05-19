b7c77fab636808b9d12c363372a4632c47d7604b
fginacio <fginacio@hotmail.com>
2023-05-19 13:40:50 -0300

    Update Test_iDrac_Connections.ps1

--- Useful Scripts/Test_iDrac_Connections.ps1
+++ Useful Scripts/Test_iDrac_Connections.ps1
@@ -1,12 +1,12 @@
 Clear
 
-ForEach ($computer in Get-Content -path "C:\Users\t3122026\Desktop\idracs.txt" ) {
+ForEach ($computer in Get-Content -path "C:\Dell\iDracs.txt" ) {
 	Write-host "Testing iDrac connectivity " $computer
 
 
 if (Test-Connection -ComputerName $computer -Quiet -Count 1) {
 	Write-Host "Testing iDrac"
-	$computer | Out-File -FilePath C:\Users\t3122026\Desktop\idracs_ok.txt -append -Encoding utf8
+	$computer | Out-File -FilePath C:\Dell\iDracs_ok.txt -append -Encoding utf8
 	Write-Host "iDrac OK!" -ForegroundColor Yellow -BackgroundColor DarkGreen
 	Write-Host "Tests Completed!"
 	Write-Host
@@ -14,7 +14,7 @@ if (Test-Connection -ComputerName $computer -Quiet -Count 1) {
 }
 
 else {
-	$computer | Out-File -FilePath C:\Users\t3122026\Desktop\idracs_BAD.txt -append -Encoding utf8
+	$computer | Out-File -FilePath C:\Dell\iDracs_BAD.txt -append -Encoding utf8
 	Write-Host
 	Write-Host "Warning: iDrac" $computer "not working! Next..." -ForegroundColor Red -BackgroundColor Yellow}
 }
