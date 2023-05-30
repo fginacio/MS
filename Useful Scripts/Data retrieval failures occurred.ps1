517449d4390119363a62a82c7260899bcf6e012b
fginacio <fginacio@hotmail.com>
2023-05-30 19:39:06 -0300

    Create Data retrieval failures occurred.ps1
    V1.0

---
+++ Useful Scripts/Data retrieval failures occurred.ps1
@@ -0,0 +1,23 @@
+#Server Manager problem: Online - Data retrieval failures occurred#
+
+#To set Enable to 0
+
+icm -ComputerName (Get-ClusterNode).name -ScriptBlock {
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-Kernel-IoTrace/Diagnostic" -Name "Enabled" -Value 0; 
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SPB-HIDI2C/Analytic" -Name "Enabled" -Value 0; 
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Debug" -Name "Enabled" -Value 0; 
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Diagnostic" -Name "Enabled" -Value 0; 
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Operational" -Name "Enabled" -Value 0;
+Set-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\NIS-Driver-WFP/Diagnostic" -Name "Enabled" -Value 0}
+
+
+#To remove the Enable key
+
+icm -ComputerName (Get-ClusterNode).name -ScriptBlock
+{Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-Kernel-IoTrace/Diagnostic" -Name "Enabled" ;
+Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SPB-HIDI2C/Analytic" -Name "Enabled" ;
+Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Debug" -Name "Enabled" ;
+Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Diagnostic" -Name "Enabled" ;
+Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\Microsoft-Windows-SystemSettingsThreshold/Operational" -Name "Enabled" ;
+Remove-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT\Channels\NIS-Driver-WFP/Diagnostic" -Name "Enabled"}
+
