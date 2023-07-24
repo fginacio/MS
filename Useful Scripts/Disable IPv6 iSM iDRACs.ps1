37c4d4965a9b922680f2165f393821f7734e961e
fginacio <fginacio@hotmail.com>
2023-07-24 15:11:03 -0300

    Update Disable IPv6 iSM iDRACs.ps1

--- Useful Scripts/Disable IPv6 iSM iDRACs.ps1
+++ Useful Scripts/Disable IPv6 iSM iDRACs.ps1
@@ -7,3 +7,15 @@
 icm -ComputerName (Get-ClusterNode).name -ScriptBlock {Disable-NetAdapterBinding –InterfaceAlias “Ethernet” –ComponentID ms_tcpip6 -verbose}
 
 #Reopen the FCM to refresh
+
+
+
+
+
+
+#Option2
+
+#Run on all nodes to exclude Remote NDIS NIC from Cluster Networks
+New-item -Path HKLM:\system\currentcontrolset\services\clussvc\parameters -Verbose
+New-ItemProperty -Path HKLM:\system\CurrentControlSet\Services\clussvc\parameters -Name ExcludeAdaptersByDescription -Value "Remote NDIS Compatible Device" -verbose
+Get-ItemProperty -Path HKLM:\system\CurrentControlSet\Services\clussvc\parameters -Name ExcludeAdaptersByDescription | Fl ExcludeAdaptersByDescription -Verbose
