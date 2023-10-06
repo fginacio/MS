bc72d265b4974fdcb8193fce8882b5c16cde1cc8
fginacio <fginacio@hotmail.com>
2023-10-06 19:54:07 -0300

    Update TSS_Collect_beta.ps1

--- TSS_Collect_beta.ps1
+++ TSS_Collect_beta.ps1
@@ -260,8 +260,8 @@ Function DisplayMenu {
             Move-Item -Path C:\Dell\SDP_DOM\*DOM.zip -Destination "c:\Dell\$CaseNumber.zip"
 
             Remove-Item "C:\Dell\SDP_*" -Recurse -Force -ErrorAction Ignore
-           #$Shell = New-Object -ComObject "WScript.Shell"
-           #$Button = $Shell.Popup("Logs available at $dell$CaseNumber .zip",0,"Collection Successfull",0)
+            $Shell = New-Object -ComObject "WScript.Shell"
+            $Button = $Shell.Popup("Logs available at $dell$CaseNumber .zip",0,"Collection Successfull",0)
             Start-Sleep -Seconds 2
             Remove-Variable CaseNumber
             Write-Host "Bye"
