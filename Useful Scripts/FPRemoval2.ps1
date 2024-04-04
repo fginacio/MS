690a2d0dc2d6c7c368fa18a2e1d71e118e77b7df
fginacio <fginacio@hotmail.com>
2024-04-04 10:29:26 -0300

    .

---
+++ Useful Scripts/FPRemoval2.ps1
@@ -0,0 +1,10 @@
+﻿set-executionpolicy bypass -scope Process -Force
+$bio="C:\Windows\System32\WinBioDatabase\*.DAT"
+Stop-Service WbioSrvc -force
+clear-content -path $bio -Force
+Start-Service WbioSrvc
+
+$Shell = New-Object -ComObject "WScript.Shell"
+$Button = $Shell.Popup("Database da Biometrica limpa com sucesso!",0)
+
+exit
