3064cad8ad9e7122b4ecfb7971bb8168d176392d
fginacio <fginacio@hotmail.com>
2024-05-14 22:27:04 -0300

    Update TSSCollect.ps1

--- DEV/TSSCollect.ps1
+++ DEV/TSSCollect.ps1
@@ -17,16 +17,12 @@ V 1.4
     Renamed main script to TSS.ps1
     Output logs available at c:\Dell\Logs
 #>
-
-
 Function Invoke-TssCollect {
 Function Check-FreeSpace {
     # Get the free space of the C:\ drive in gigabytes
     $freeSpaceGB = [math]::Round((Get-WmiObject Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace / 1GB, 2)
-
     # Define the threshold
     $thresholdGB = 1
-
     # Check if free space is less than the threshold
     if ($freeSpaceGB -lt $thresholdGB) {
         $Shell = New-Object -ComObject "WScript.Shell"
@@ -45,14 +41,11 @@ Function Check-FreeSpace {
 Function EndScript {
 Break
 }
-
 Function DisplayMenu {
     $DateTime = Get-Date -Format yyyyMMdd_HHmmss
     Start-Transcript -NoClobber -Path "C:\Dell\TssCollect_$DateTime.log"
     clear-host
-
     Write-Host @"
-
     +======================================================================+
     |                                                                      |
     |                                                                      |                
@@ -68,7 +61,6 @@ Function DisplayMenu {
     |                                                                      |
     |                                                                      |
     +======================================================================+
-
 "@
     Write-Host "Below symbols are not allowed." -ForegroundColor Yellow -BackgroundColor DarkGray
     Write-Host "=> Illegal characters/symbols: #<>*_/\{}$+%`|=@\" -ForegroundColor Yellow -BackgroundColor DarkGray
@@ -85,19 +77,10 @@ Function DisplayMenu {
     Y {
     IF      (get-WindowsFeature -Name Failover-clustering | where Installed) {
     #OPTION - Cluster Collection#
-            try {
+            
             # Attempt to execute the command
             Invoke-Expression -Command "C:\dell\Tss\TSS.ps1 -sdp Cluster -LogFolderPath $dell -AcceptEula"
             Set-Location $tss
-            } catch {
-                # Check if the error message indicates disk full
-                if ($_.Exception.Message -like "*disk*full*") {
-                    Write-Host "Error: Disk is full."
-                    Write-Host "Checking free space on C: drive..."
-                    Check-FreeSpace
-                }
-            }   
-            
             #Compressing logs#
             clear-host
             $sourceFolder = "C:\Dell\SDP_Cluster\"
@@ -117,26 +100,17 @@ Function DisplayMenu {
             }
     ELSEIF  (get-WindowsFeature -Name Hyper-V | where Installed) {
     #OPTION - HyperV Collection#
-            try {
+            
             # Attempt to execute the command
             Invoke-Expression -Command "C:\dell\Tss\TSS.ps1 -sdp HyperV -LogFolderPath $dell -AcceptEula"
             Set-Location $tss
-            } catch {
-                # Check if the error message indicates disk full
-                if ($_.Exception.Message -like "*disk*full*") {
-                    Write-Host "Error: Disk is full."
-                    Write-Host "Checking free space on C: drive..."
-                    Check-FreeSpace
-                }
-            }   
-            
             #Compressing logs#
             clear-host
             $sourceFolder = "C:\Dell\SDP_HyperV\"
             Write-Host "Compressing $sourceFolder folder to " c:\Dell\Logs\$CaseNumber.zip". This might take a while."
             $logtemp = Get-ChildItem -Path c:\Dell\SDP_HyperV\*HyperV.zip
             Move-Item -Path c:\Dell\SDP_HyperV\*HyperV.zip -Destination "c:\Dell\Logs\$CaseNumber.zip"
-            
+               
             Remove-Item "C:\Dell\SDP_*" -Recurse -Force -ErrorAction Ignore
             $Shell = New-Object -ComObject "WScript.Shell"
             $Button = $Shell.Popup("$Button at c:\Dell\Logs",0,"Collection Successfull",0)
@@ -149,24 +123,17 @@ Function DisplayMenu {
             }
     ELSEIF  (get-WindowsFeature -Name AD-Domain-Services | where Installed) {
     #OPTION - Active Directory Collection#
-            try {
+            
             # Attempt to execute the command
             Invoke-Expression -Command "C:\dell\Tss\TSS.ps1 -sdp DOM -LogFolderPath $dell -AcceptEula"
             Set-Location $tss
-            } catch {
-                # Check if the error message indicates disk full
-                if ($_.Exception.Message -like "There is not enough space on the disk") {
-                    Write-Host "Error: Disk is full."
-                    Check-FreeSpace
-                }
-
             #Compressing logs#
             clear-host
             $sourceFolder = "C:\Dell\SDP_DOM\"
             Write-Host "Compressing $sourceFolder folder to " c:\Dell\Logs\$CaseNumber.zip". This might take a while."
             $logtemp = Get-ChildItem -Path C:\Dell\SDP_DOM\*DOM.zip
             Move-Item -Path C:\Dell\SDP_DOM\*DOM.zip -Destination "c:\Dell\Logs\$CaseNumber.zip"
-           
+            
             Remove-Item "C:\Dell\SDP_*" -Recurse -Force -ErrorAction Ignore
             $Shell = New-Object -ComObject "WScript.Shell"
             $Button = $Shell.Popup("$Button at c:\Dell\Logs",0,"Collection Successfull",0)
@@ -179,26 +146,17 @@ Function DisplayMenu {
             }
     ELSE {
     #OPTION - Default Collection#
-            try {
+            
             # Attempt to execute the command 
             Invoke-Expression -Command "C:\dell\Tss\TSS.ps1 -sdp Setup -LogFolderPath $dell -AcceptEula"
             Set-Location $tss
-            } catch {
-                # Check if the error message indicates disk full
-                if ($_.Exception.Message -like "*disk*full*") {
-                    Write-Host "Error: Disk is full."
-                    Write-Host "Checking free space on C: drive..."
-                    Check-FreeSpace
-                }
-            }   
-            
             #Compressing logs#
             clear-host
             $sourceFolder = "C:\Dell\SDP_Setup\"
             Write-Host "Compressing $sourceFolder folder to " c:\Dell\Logs\$CaseNumber.zip". This might take a while."
             $logtemp = Get-ChildItem -Path C:\Dell\SDP_Setup\*Setup.zip
             Move-Item -Path C:\Dell\SDP_Setup\*Setup.zip -Destination "c:\Dell\Logs\$CaseNumber.zip"
-           
+            
             Remove-Item "C:\Dell\SDP_*" -Recurse -Force -ErrorAction Ignore
             $Shell = New-Object -ComObject "WScript.Shell"
             $Button = $Shell.Popup("Logs available at c:\Dell\Logs",0,"Collection Successfull",0)
@@ -209,7 +167,6 @@ Function DisplayMenu {
             Stop-Transcript
             EndScript
 }
-
             }
         N {
             #OPTIONQ - EXIT#
@@ -224,14 +181,12 @@ Function DisplayMenu {
             DisplayMenu
         }
     }
-
 }
 Set-PSDebug -Trace 0
 $dell = "c:\Dell\"
 $TSS = "C:\dell\Tss\"
 clear-host
 Write-Host "Downloading and Unpacking Tss..."
-
 #Deleting old log collections and transcript logs#
 Remove-Item "C:\Dell\Dumps" -Recurse -Force -ErrorAction Ignore
 Remove-Item "C:\Dell\Logs" -Recurse -Force -ErrorAction Ignore
@@ -239,29 +194,22 @@ Remove-Item "C:\Dell\SDP_*" -Recurse -Force -ErrorAction Ignore
 Remove-Item "C:\Dell\TssCollect*.log" -Recurse -Force -ErrorAction Ignore
 Remove-Item "C:\Dell\Tss" -Recurse -Force -ErrorAction Ignore
 Remove-Item "C:\Dell\Tss.zip" -Recurse -Force -ErrorAction Ignore
-
 #Checking requirements to run (Minimun Powershell 5.1)#
 $ps = ($PSVersionTable).PSVersion.Major
-
 #Creating c:\Dell folder and downloading Tss#
 New-Item -Path C:\Dell\Tss -ItemType Directory
 New-Item -Path C:\Dell\Logs -ItemType Directory
 Start-Sleep -Seconds 5
-
 Check-FreeSpace
-
 #Downloading TSS#
 wget http://aka.ms/getTss -OutFile "c:\Dell\Tss.zip" -ErrorAction SilentlyContinue
 #wget https://github.com/fginacio/MS/raw/main/Tss.zip -OutFile c:\Dell\Tss.zip
-
-
 #Unpacking Tss at C:\Dell#
 if ($ps -ge 5)
     {
         [Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
         $zip = [System.IO.Compression.ZipFile]::Open("c:\dell\Tss.zip",'read')
         [System.IO.Compression.ZipFileExtensions]::ExtractToDirectory($zip,"C:\Dell\Tss")
-
     }
 else
 {
@@ -277,31 +225,23 @@ else
     Write-Host $ErrorMSG -ForegroundColor Red -BackgroundColor Yellow
     EndScript
 }
-
 clear-host
 $Ver = "1.4"
-
 #IE Fix#
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
-
 #Set Execution Policy#
 #$ExecutionPolicy = Get-ExecutionPolicy
 Write-Host "Set ExecutionPolicy Bypass..."
     Set-ExecutionPolicy Bypass -Scope Process -Force
 Write-Host "    ExecutionPolicy:"$env:PSExecutionPolicyPreference
-
 #Detect Roles/Features#
 $roles= get-WindowsFeature -Name * | where Installed
 Write-Host "Checking installed Features/Roles. " -ForegroundColor Green -BackgroundColor DarkGray
 Start-Sleep -Seconds 2
 clear-host
-
 #OPTION - BSOD Collection#
 #Copying logs#
 #Creating a temporary dumps folder#
-
-
-
             $DumpFolder = "c:\dell\dumps"
             if (Test-Path "$DumpFolder")
                 {
@@ -311,17 +251,13 @@ clear-host
             else
                 {
                     #Write-Host "Folder Doesn't Exists, creating..."
-
                     #PowerShell Create directory if not exists
                     New-Item $DumpFolder -ItemType Directory
                 }
-
-
         # Checking dump file timestamp is more than 30 Days
         $memoryDmpPath = "c:\windows\memory.dmp"
         $minidumpPath = "c:\windows\minidump"
         $DumpFolder = "c:\dell\dumps"
-
         # Check if at least one of the paths exist before accessing their LastWriteTime
         if ((Test-Path $memoryDmpPath) -or (Test-Path $minidumpPath)) {
            
@@ -330,102 +266,70 @@ clear-host
                 $memoryDmpTimestamp = (Get-Item $memoryDmpPath).LastWriteTime
                 $currentTimestamp = Get-Date
                 $daysDifference = ($currentTimestamp - $memoryDmpTimestamp).Days
-
                 # Copy Memory.dmp if the conditions are met
                 if ($daysDifference -lt 30) {
                     Copy-Item -Path $memoryDmpPath -Destination $DumpFolder -Recurse -ErrorAction SilentlyContinue
                 }
             }
-
             # Check if the file exists before accessing its LastWriteTime
             if (Test-Path $minidumpPath) {
                 $minidumpPathTimestamp = (Get-Item $minidumpPath).LastWriteTime
                 $currentTimestamp = Get-Date
                 $daysDifference2 = ($currentTimestamp - $minidumpPathTimestamp).Days
-
                 # Copy files from minidump folder if the conditions are met
                 if ($daysDifference2 -lt 30) {
                     Copy-Item -Path $minidumpPath -Destination $DumpFolder -Recurse -ErrorAction SilentlyContinue
                 }
             }
         }
-
 # Compressing logs #
 $DumpFolder = "c:\dell\dumps"
 $DestPath = "C:\Dell\logs\dump.zip"
 $maximumFileSize = 4GB
-
 # Check if there are files in the folder before proceeding with compression
 $files = Get-ChildItem -Path $DumpFolder -File -Recurse -Force
-
 if ($files.Count -gt 0) {
     # Give some time for logging to complete before starting zip
     Start-Sleep -Seconds 5
-
     $totalSize = ($files | Measure-Object -Property Length -Sum).Sum
-
     if ($totalSize -gt $maximumFileSize) {
         Write-Host "Starting a fresh compression for large files using System.IO.Compression.ZipFile class."
-
         # Create a temporary directory to store the large files
         $tempDir = New-Item -ItemType Directory -Path (Join-Path -Path $env:TEMP -ChildPath "LargeFilesTemp")
-
         # Separate large and small files
         $largeFiles = $files | Where-Object { $_.Length -gt $maximumFileSize }
         $smallFiles = $files | Where-Object { $_.Length -le $maximumFileSize }
-
         # Copy the large files to the temporary directory
         foreach ($file in $largeFiles) {
             $destinationPath = Join-Path -Path $tempDir.FullName -ChildPath $file.Name
             Copy-Item -Path $file.FullName -Destination $destinationPath -Force
         }
-
         # Create a new zip archive
-         
         [System.IO.Compression.ZipFile]::CreateFromDirectory($tempDir.FullName, $DestPath, 'Optimal', $true)
-
         # Include the small files in the zip archive
         $smallFiles | Compress-Archive -DestinationPath $DestPath -Update -CompressionLevel Optimal
-
         # Remove the temporary directory
         Remove-Item -Path $tempDir.FullName -Force -Recurse
     } else {
         # Compress all files using Compress-Archive
-        try {
         $files | Compress-Archive -DestinationPath $DestPath -Update -CompressionLevel Optimal
-
-        } catch {
-                # Check if the error message indicates disk full
-                if ($_.Exception.Message -like "*disk*full*") {
-                    Write-Host "Error: Disk is full."
-                    Write-Host "Checking free space on C: drive..."
-                    Check-FreeSpace
-                }
-            } 
-
         # Calculate the progress per file
         $progressPerFile = 100 / $files.Count
         $totalProgress = 0
-
         foreach ($file in $files) {
             $totalProgress += $progressPerFile
             Write-Progress -Activity "Compressing Files" -Status "Progress" -PercentComplete $totalProgress
         }
     }
 } 
-
 #MainMenu#
 DisplayMenu
-
 #Invoke-TssCollect#
 clear-host
 Remove-Item -Path "C:\Dell\Tss.zip" -Recurse -Force -ErrorAction Ignore
 $logfolder = (Get-ChildItem -Path c:\dell\Logs\*.zip -Name)
 Write-Host "Logs available at c:\Dell\Logs"
-
-
 #Removing extracted collector and zip file#
 Remove-Item "C:\Dell\Tss" -Recurse -Force -ErrorAction Ignore
 Remove-Item "C:\Dell\Dumps" -Recurse -Force -ErrorAction Ignore
 }
-
